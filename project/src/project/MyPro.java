ba3bde0c5359dc0c0d1ee0c2b32b0fd6199f4cb2
vandal-prog <josiahonche78@gmail.com>
2021-06-19 22:21:44 +0100

    updated

--- project/src/project/MyPro.java
+++ project/src/project/MyPro.java
@@ -4,7 +4,10 @@ public class MyPro {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello world");
+	    Test student1 = new Test("John", "smith", 90);
+	    System.out.println("full name of student1 is " + student1.getFullName());
+	    Test student2 = new Test("John", "will", 90);
+	    System.out.println("full name of student2 is " + student2.getFullName());
 
 	}
 
--- project/src/project/Test.java
+++ project/src/project/Test.java
@@ -1,5 +1,20 @@
 package project;
 
 public class Test {
+	   //instance variable
+    String firstName;
+    String lastName;
+     int marks;
 
+     //constructors
+    Test(String first, String last, int score) {
+        firstName = first;
+        lastName = last;
+        marks = score;
+    }
+
+    public String getFullName() {
+        return firstName +" "+lastName;
+        
+    }
 }
